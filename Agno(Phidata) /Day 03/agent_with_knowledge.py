60fdac03bd13b2dd0921f35771f4887cb28bd0cf
jitender-insights <138782926+jitender-insights@users.noreply.github.com>
2025-02-04 15:58:21 +0530

    Add files via upload

---
+++ Agno(Phidata) /Day 03/agent_with_knowledge.py
@@ -0,0 +1,31 @@
+from agno.agent import Agent 
+from agno.knowledge.pdf_url import PDFUrlKnowledgeBase
+from agno.models.google import Gemini
+from agno.models.groq import Groq 
+from agno.embedder.google import GeminiEmbedder
+from agno.vectordb.lancedb import LanceDb
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+os.environ["GOOGLE_API_KEY"] = os.getenv("GOOGLE_API_KEY")
+os.environ['GROQ_API_KEY'] = os.getenv("GROQ_API_KEY")
+
+knowledge_base = PDFUrlKnowledgeBase(
+    urls=["https://agno-public.s3.amazonaws.com/recipes/ThaiRecipes.pdf"],
+    vector_db= LanceDb(
+        table_name="recipes",
+        uri="tmp/lancedb",
+        embedder=GeminiEmbedder()
+    )
+)
+knowledge_base.load(recreate=True)  #Comment out after first run
+
+
+agent = Agent(
+    model=Gemini(id="gemini-2.0-flash-exp"),
+    knowledge=knowledge_base,
+    show_tool_calls=True
+)
+
+agent.print_response("How Deeseek model is better than OpenAI", markdown=True)
